43a244f25f571d948aa82a6ce3a8c159c6b0e658
Guilherme De Paiva <guilherme.ps20@gmail.com>
2016-02-21 09:26:53 -0300

    Atualização da Base de Dados para a Versão 1.5

--- BD/2 - Sequences.sql
+++ BD/2 - Sequences.sql
@@ -13,3 +13,15 @@ ALTER TABLE seq_chave_primaria
 
 ------------------------------------------------------------------------
 
+-- Sequence: seq_codigo_registro
+
+-- DROP SEQUENCE seq_codigo_registro;
+
+CREATE SEQUENCE seq_codigo_registro
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 579
+  CACHE 1;
+ALTER TABLE seq_codigo_registro
+  OWNER TO postgres;
--- BD/5 - Values.sql
+++ BD/5 - Values.sql
@@ -1,5 +1,5 @@
 --Usuários--
-INSERT INTO usuario VALUES (NEXTVAL('SEQ_CHAVE_PRIMARIA'), '08F9CE6B9E70DADB981D0E8249CB9042', 'Guilherme de Paiva Santos', 1, 'guilherme', '11122233345', TRUE);
+INSERT INTO usuario VALUES (NEXTVAL('SEQ_CHAVE_PRIMARIA'), '08F9CE6B9E70DADB981D0E8249CB9042', 'Guilherme de Paiva Santos', 1, 'guilherme', '11122233345', TRUE, TRUE);
 
 --Classes de Veículos e Componentes--
 INSERT INTO classe VALUES (NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Automóvel', TRUE), 
@@ -47,5 +47,5 @@ INSERT INTO cor VALUES (NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Branco'),
 		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Rosa');
 
 --Modelos--
-INSERT INTO modelo VALUES (NEXTVAL('SEQ_CHAVE_PRIMARIA'), (SELECT id_classe FROM classe LIMIT 1), 'Gol', 'Volkswagem', '4 portas'), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'),(SELECT id_classe FROM classe WHERE vc_nome_classe = 'Caminhonete'),'Hilux SRV 11/12','Toyota','Automática 3.0 Diesel 4x4 - 2011');
+INSERT INTO modelo VALUES (NEXTVAL('SEQ_CHAVE_PRIMARIA'), (SELECT id_classe FROM classe LIMIT 1), NEXTVAL('SEQ_CODIGO_REGISTRO'), 'Gol', 'Volkswagem', '4 portas'), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), (SELECT id_classe FROM classe WHERE vc_nome_classe = 'Caminhonete'), NEXTVAL('SEQ_CODIGO_REGISTRO'), 'Hilux SRV 11/12','Toyota','Automática 3.0 Diesel 4x4 - 2011');
