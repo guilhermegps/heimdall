212e5db738f6811bea918834c5d205b2e0e1e5bd
Guilherme De Paiva <guilherme.ps20@gmail.com>
2016-09-04 14:19:10 -0300

    Atualização da Base de Dados para a Versão 2.0

--- BD/1 - Tables.sql
+++ BD/1 - Tables.sql
@@ -1,122 +1,240 @@
-CREATE TABLE cor (
-  id_cor INTEGER NOT NULL,
-  vc_nome_cor VARCHAR(50) NOT NULL,
-  PRIMARY KEY(id_cor)
-);
 
-CREATE TABLE usuario (
-  id_usuario INTEGER NOT NULL,
-  vc_senha_usuario VARCHAR(32) NOT NULL,
-  vc_nome_usuario VARCHAR(80) NOT NULL,
-  in_nivel_usuario INTEGER NOT NULL,
-  vc_login_usuario VARCHAR(50) NOT NULL,
-  vc_cpf_usuario VARCHAR(11) NOT NULL,
-  bo_primeiro_login_usuario BOOLEAN NOT NULL,
-  bo_registro_ativo_usuario BOOLEAN NOT NULL,
-  PRIMARY KEY(id_usuario),
-  UNIQUE(vc_login_usuario),
-  UNIQUE(vc_cpf_usuario)
+CREATE TABLE public.usuario (
+                id_usuario INTEGER NOT NULL,
+                vc_senha_usuario VARCHAR(32) NOT NULL,
+                vc_nome_usuario VARCHAR(80) NOT NULL,
+                nu_nivel_usuario INTEGER NOT NULL,
+                vc_login_usuario VARCHAR(50) NOT NULL,
+                vc_cpf_usuario VARCHAR(11) NOT NULL,
+                bo_primeiro_login_usuario BOOLEAN NOT NULL,
+                bo_registro_ativo_usuario BOOLEAN NOT NULL,
+                bo_revisa_sem_agendamento VARCHAR NOT NULL,
+                CONSTRAINT usuario_pkey PRIMARY KEY (id_usuario)
 );
+COMMENT ON COLUMN public.usuario.nu_nivel_usuario IS 'Níveis de usuários:
+1 - Gestor
+2 - Administrador
+3 - Funcionário';
 
-CREATE TABLE classe (
-  id_classe INTEGER NOT NULL,
-  vc_nome_classe VARCHAR(50) NOT NULL,
-  bo_tipo_classe BOOLEAN NOT NULL,
-  PRIMARY KEY(id_classe)
-);
 
-CREATE TABLE modelo (
-  id_modelo INTEGER NOT NULL,
-  classe_id_classe INTEGER NOT NULL,
-  nu_codigo_modelo INTEGER NOT NULL,
-  vc_descricao_modelo VARCHAR(80) NOT NULL,
-  vc_marca_modelo VARCHAR(50) NOT NULL,
-  vc_layout_modelo VARCHAR(80) NULL,
-  PRIMARY KEY(id_modelo),
-  FOREIGN KEY(classe_id_classe)
-    REFERENCES classe(id_classe),
-  UNIQUE(nu_codigo_modelo)
-);
+CREATE UNIQUE INDEX usuario_vc_cpf_usuario_key
+ ON public.usuario USING BTREE
+ ( vc_cpf_usuario );
 
-CREATE TABLE log_usuario (
-  id_logs INTEGER NOT NULL,
-  usuario_id_usuario INTEGER NOT NULL,
-  dh_registro_log TIMESTAMP NOT NULL,
-  id_origem_log INTEGER NOT NULL,
-  tx_acao_log VARCHAR NOT NULL,
-  vc_nome_origem_log VARCHAR(80) NOT NULL,
-  PRIMARY KEY(id_logs),
-  FOREIGN KEY(usuario_id_usuario)
-    REFERENCES usuario(id_usuario)
-);
+CREATE UNIQUE INDEX usuario_vc_login_usuario_key
+ ON public.usuario USING BTREE
+ ( vc_login_usuario );
 
-CREATE TABLE veiculo (
-  id_veiculo INTEGER NOT NULL,
-  modelo_id_modelo INTEGER NOT NULL,
-  cor_id_cor INTEGER NULL,
-  nu_codigo_veiculo INTEGER NOT NULL,
-  vc_nome_veiculo VARCHAR(50) NOT NULL,
-  vc_rfid_veiculo VARCHAR(11) NULL,
-  vc_placa_veiculo VARCHAR(8) NOT NULL,
-  nu_km_veiculo NUMERIC NULL,
-  tx_observacao_veiculo TEXT NULL,
-  dh_registro_veiculo TIMESTAMP NOT NULL,
-  bo_registro_ativo_veiculo BOOLEAN NOT NULL,
-  PRIMARY KEY(id_veiculo),
-  FOREIGN KEY(cor_id_cor)
-    REFERENCES cor(id_cor),
-  FOREIGN KEY(modelo_id_modelo)
-    REFERENCES modelo(id_modelo),
-  UNIQUE(nu_codigo_veiculo),
-  UNIQUE(vc_rfid_veiculo)
-);
-
-CREATE TABLE revisao (
-  id_revisao INTEGER NOT NULL,
-  usuario_id_usuario INTEGER NOT NULL,
-  veiculo_id_veiculo INTEGER NOT NULL,
-  nu_numero_revisao INTEGER NOT NULL,
-  dh_registro_revisao TIMESTAMP NOT NULL,
-  tx_descricao_revisao TEXT NULL,
-  PRIMARY KEY(id_revisao),
-  FOREIGN KEY(veiculo_id_veiculo)
-    REFERENCES veiculo(id_veiculo),
-  FOREIGN KEY(usuario_id_usuario)
-    REFERENCES usuario(id_usuario),
-  UNIQUE(nu_numero_revisao)
-);
-
-CREATE TABLE componente (
-  id_componente INTEGER NOT NULL,
-  modelo_id_modelo INTEGER NOT NULL,
-  veiculo_id_veiculo INTEGER NOT NULL,
-  nu_codigo_componente INTEGER NOT NULL,
-  vc_rfid_componente VARCHAR(11) NOT NULL,
-  vc_nome_componente VARCHAR(50) NOT NULL,
-  tx_descricao_componente TEXT NULL,
-  dh_validade_componente TIMESTAMP NULL,
-  dh_registro_componente TIMESTAMP NOT NULL,
-  bo_registro_ativo_componente BOOLEAN NOT NULL,
-  PRIMARY KEY(id_componente),
-  FOREIGN KEY(veiculo_id_veiculo)
-    REFERENCES veiculo(id_veiculo),
-  FOREIGN KEY(modelo_id_modelo)
-    REFERENCES modelo(id_modelo),
-  UNIQUE(nu_codigo_componente),
-  UNIQUE(vc_rfid_componente)
-);
-
-CREATE TABLE componente_revisao (
-  componente_id_componente INTEGER NOT NULL,
-  revisao_id_revisao INTEGER NOT NULL,
-  bo_identificado BOOLEAN NOT NULL,
-  dh_identificacao_componente_revisao TIMESTAMP NULL,
-  tx_motivo_nao_identificacao TEXT NULL,
-  PRIMARY KEY(componente_id_componente, revisao_id_revisao),
-  FOREIGN KEY(componente_id_componente)
-    REFERENCES componente(id_componente),
-  FOREIGN KEY(revisao_id_revisao)
-    REFERENCES revisao(id_revisao)
+CREATE TABLE public.log_usuario (
+                id_log INTEGER NOT NULL,
+                id_usuario INTEGER NOT NULL,
+                dh_registro_log TIMESTAMP NOT NULL,
+                tx_acao_log TEXT NOT NULL,
+                CONSTRAINT log_usuario_pkey PRIMARY KEY (id_log)
 );
 
 
+CREATE TABLE public.cor (
+                id_cor INTEGER NOT NULL,
+                vc_nome_cor VARCHAR(50) NOT NULL,
+                CONSTRAINT cor_pkey PRIMARY KEY (id_cor)
+);
+
+
+CREATE TABLE public.classe (
+                id_classe INTEGER NOT NULL,
+                vc_nome_classe VARCHAR(50) NOT NULL,
+                nu_tipo_classe INTEGER NOT NULL,
+                CONSTRAINT classe_pkey PRIMARY KEY (id_classe)
+);
+COMMENT ON COLUMN public.classe.nu_tipo_classe IS '1 - Veiculo
+2 - Componente';
+
+
+CREATE TABLE public.modelo (
+                id_modelo INTEGER NOT NULL,
+                id_classe INTEGER NOT NULL,
+                nu_codigo_modelo INTEGER NOT NULL,
+                tx_descricao_modelo TEXT NOT NULL,
+                vc_marca_modelo VARCHAR(80) NOT NULL,
+                vc_layout_modelo VARCHAR(80),
+                CONSTRAINT modelo_pkey PRIMARY KEY (id_modelo)
+);
+
+
+CREATE UNIQUE INDEX modelo_nu_codigo_modelo_key
+ ON public.modelo USING BTREE
+ ( nu_codigo_modelo );
+
+CREATE TABLE public.veiculo (
+                id_veiculo INTEGER NOT NULL,
+                id_modelo INTEGER NOT NULL,
+                id_cor INTEGER,
+                nu_codigo_veiculo INTEGER NOT NULL,
+                vc_nome_veiculo VARCHAR(50) NOT NULL,
+                vc_rfid_veiculo VARCHAR(11),
+                vc_placa_veiculo VARCHAR(8) NOT NULL,
+                nu_km_veiculo NUMERIC(13),
+                tx_observacao_veiculo TEXT,
+                dh_registro_veiculo TIMESTAMP NOT NULL,
+                bo_registro_ativo_veiculo BOOLEAN NOT NULL,
+                CONSTRAINT veiculo_pkey PRIMARY KEY (id_veiculo)
+);
+
+
+CREATE UNIQUE INDEX veiculo_nu_codigo_veiculo_key
+ ON public.veiculo USING BTREE
+ ( nu_codigo_veiculo );
+
+CREATE UNIQUE INDEX veiculo_vc_rfid_veiculo_key
+ ON public.veiculo USING BTREE
+ ( vc_rfid_veiculo );
+
+CREATE TABLE public.revisao (
+                id_revisao INTEGER NOT NULL,
+                id_veiculo INTEGER NOT NULL,
+                id_usuario INTEGER NOT NULL,
+                nu_numero_revisao INTEGER NOT NULL,
+                dh_execucao_revisao TIMESTAMP,
+                tx_descricao_revisao TEXT,
+                id_usuario_autorizador INTEGER NOT NULL,
+                bo_executada BOOLEAN NOT NULL,
+                CONSTRAINT revisao_pkey PRIMARY KEY (id_revisao)
+);
+
+
+CREATE TABLE public.agenda_revisao (
+                id_agenda_revisao INTEGER NOT NULL,
+                dh_data_inicio TIMESTAMP NOT NULL,
+                dh_data_termino VARCHAR NOT NULL,
+                hr_hora_inicio TIME NOT NULL,
+                hr_hora_termino TIME NOT NULL,
+                in_intervalo INTERVAL,
+                bo_segunda BOOLEAN NOT NULL,
+                bo_terca BOOLEAN NOT NULL,
+                bo_quarta BOOLEAN NOT NULL,
+                bo_quinta BOOLEAN NOT NULL,
+                bo_sexta BOOLEAN NOT NULL,
+                bo_sabado BOOLEAN NOT NULL,
+                bo_domingo BOOLEAN NOT NULL,
+                bo_feriados BOOLEAN NOT NULL,
+                CONSTRAINT id_agenda_revisao PRIMARY KEY (id_agenda_revisao)
+);
+
+
+CREATE TABLE public.componente (
+                id_componente INTEGER NOT NULL,
+                id_modelo INTEGER NOT NULL,
+                id_veiculo INTEGER NOT NULL,
+                nu_codigo_componente INTEGER NOT NULL,
+                vc_rfid_componente VARCHAR(11) NOT NULL,
+                vc_nome_componente VARCHAR(50) NOT NULL,
+                tx_descricao_componente TEXT,
+                dh_validade_componente TIMESTAMP,
+                dh_registro_componente TIMESTAMP NOT NULL,
+                bo_registro_ativo_componente BOOLEAN NOT NULL,
+                CONSTRAINT componente_pkey PRIMARY KEY (id_componente)
+);
+
+
+CREATE UNIQUE INDEX componente_nu_codigo_componente_key
+ ON public.componente USING BTREE
+ ( nu_codigo_componente );
+
+CREATE UNIQUE INDEX componente_vc_rfid_componente_key
+ ON public.componente USING BTREE
+ ( vc_rfid_componente );
+
+CREATE TABLE public.componente_revisao (
+                id_componente INTEGER NOT NULL,
+                id_revisao INTEGER NOT NULL,
+                bo_identificado BOOLEAN NOT NULL,
+                dh_identificacao_componente_revisao TIMESTAMP,
+                tx_motivo_nao_identificacao TEXT,
+                CONSTRAINT componente_revisao_pkey PRIMARY KEY (id_componente, id_revisao)
+);
+
+
+ALTER TABLE public.log_usuario ADD CONSTRAINT log_usuario_usuario_id_usuario_fkey
+FOREIGN KEY (id_usuario)
+REFERENCES public.usuario (id_usuario)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.revisao ADD CONSTRAINT revisao_usuario_id_usuario_fkey
+FOREIGN KEY (id_usuario_autorizador)
+REFERENCES public.usuario (id_usuario)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.revisao ADD CONSTRAINT usuario_revisao_fk
+FOREIGN KEY (id_usuario)
+REFERENCES public.usuario (id_usuario)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.veiculo ADD CONSTRAINT veiculo_cor_id_cor_fkey
+FOREIGN KEY (id_cor)
+REFERENCES public.cor (id_cor)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.modelo ADD CONSTRAINT modelo_classe_id_classe_fkey
+FOREIGN KEY (id_classe)
+REFERENCES public.classe (id_classe)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.componente ADD CONSTRAINT componente_modelo_id_modelo_fkey
+FOREIGN KEY (id_modelo)
+REFERENCES public.modelo (id_modelo)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.veiculo ADD CONSTRAINT veiculo_modelo_id_modelo_fkey
+FOREIGN KEY (id_modelo)
+REFERENCES public.modelo (id_modelo)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.componente ADD CONSTRAINT componente_veiculo_id_veiculo_fkey
+FOREIGN KEY (id_veiculo)
+REFERENCES public.veiculo (id_veiculo)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.revisao ADD CONSTRAINT revisao_veiculo_id_veiculo_fkey
+FOREIGN KEY (id_veiculo)
+REFERENCES public.veiculo (id_veiculo)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.componente_revisao ADD CONSTRAINT componente_revisao_revisao_id_revisao_fkey
+FOREIGN KEY (id_revisao)
+REFERENCES public.revisao (id_revisao)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.agenda_revisao ADD CONSTRAINT revisao_agenda_revisao_fk
+FOREIGN KEY (id_agenda_revisao)
+REFERENCES public.revisao (id_revisao)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.componente_revisao ADD CONSTRAINT componente_revisao_componente_id_componente_fkey
+FOREIGN KEY (id_componente)
+REFERENCES public.componente (id_componente)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
--- BD/5 - Values.sql
+++ BD/5 - Values.sql
@@ -1,39 +1,39 @@
 --Usuários--
 INSERT INTO usuario(
-            id_usuario, vc_senha_usuario, vc_nome_usuario, in_nivel_usuario, 
+            id_usuario, vc_senha_usuario, vc_nome_usuario, nu_nivel_usuario, 
             vc_login_usuario, vc_cpf_usuario, bo_primeiro_login_usuario, 
-            bo_registro_ativo_usuario) VALUES (NEXTVAL('SEQ_CHAVE_PRIMARIA'), '1BADB7678EA292D3E42439D07B6AFEAC', 'Guilherme de Paiva Santos', 1, 'admin', '11122233345', TRUE, TRUE);
+            bo_registro_ativo_usuario, bo_revisa_sem_agendamento) VALUES (NEXTVAL('SEQ_CHAVE_PRIMARIA'), '1BADB7678EA292D3E42439D07B6AFEAC', 'Guilherme de Paiva Santos', 1, 'admin', '11122233345', TRUE, TRUE, TRUE);
 
 --Classes de Veículos e Componentes--
 INSERT INTO classe(
-            id_classe, vc_nome_classe, bo_tipo_classe) VALUES (NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Automóvel', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Caminhão', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Caminhão Trator', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Caminhonete', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Camioneta', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Chassi Plataforma', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Ciclomotor', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Microônibus', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Motocicleta', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Motoneta', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Ônibus', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Quadriciclo', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Reboque', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Semi-reboque', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Side-car', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Trator Esteira', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Trator Rodas', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Triciclo', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Utilitário', TRUE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Amortecedor', FALSE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Roda', FALSE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Pastilha de Freio', FALSE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Pneu', FALSE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Calota', FALSE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Farol/Lanterna', FALSE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Rack/Bagageiro', FALSE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Bateria', FALSE), 
-		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Parachoque', FALSE);
+            id_classe, vc_nome_classe, nu_tipo_classe) VALUES (NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Automóvel', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Caminhão', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Caminhão Trator', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Caminhonete', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Camioneta', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Chassi Plataforma', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Ciclomotor', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Microônibus', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Motocicleta', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Motoneta', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Ônibus', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Quadriciclo', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Reboque', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Semi-reboque', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Side-car', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Trator Esteira', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Trator Rodas', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Triciclo', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Utilitário', 1), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Amortecedor', 2), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Roda', 2), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Pastilha de Freio', 2), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Pneu', 2), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Calota', 2), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Farol/Lanterna', 2), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Rack/Bagageiro', 2), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Bateria', 2), 
+		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), 'Parachoque', 2);
 
 --Cores--
 INSERT INTO cor(
@@ -52,7 +52,7 @@ INSERT INTO cor(
 
 --Modelos--
 INSERT INTO modelo(
-            id_modelo, classe_id_classe, nu_codigo_modelo, vc_descricao_modelo, 
+            id_modelo, id_classe, nu_codigo_modelo, tx_descricao_modelo, 
             vc_marca_modelo, vc_layout_modelo) VALUES (NEXTVAL('SEQ_CHAVE_PRIMARIA'), (SELECT id_classe FROM classe LIMIT 1), NEXTVAL('SEQ_CODIGO_REGISTRO'), 'Gol', 'Volkswagem', '4 portas'), 
 		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), (SELECT id_classe FROM classe WHERE vc_nome_classe = 'Caminhonete'), NEXTVAL('SEQ_CODIGO_REGISTRO'), 'Hilux SRV 11/12','Toyota','Automática 3.0 Diesel 4x4 - 2011'),
 		(NEXTVAL('SEQ_CHAVE_PRIMARIA'), (SELECT id_classe FROM classe WHERE vc_nome_classe = 'Pneu'), NEXTVAL('SEQ_CODIGO_REGISTRO'),'175/65r14 ','Pirelli','82t Formula Spider');
